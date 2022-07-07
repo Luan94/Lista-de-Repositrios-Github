86efdb5ab79faed37cfd3e02ab6e58fdfa15010a
Luan94 <luanvilas0@gmail.com>
2022-07-07 16:28:49 -0300

    Começo da criação da funcionalidade de filtrar os cartões

--- src/App.js
+++ src/App.js
@@ -12,9 +12,46 @@ const GitApiConfig = {
   secret: "302d94f3c76a06f03b5f2a70fcefa90dcae88c9e"
 }
 
-const Axios = () => {
-  const [GitRepoApi, setGitRepoApi] = useState([]);
 
+
+const Axios = () => {
+
+  const [clicked, setClicked] = useState('');
+
+  const handleClick = event => {
+
+    const type = event.currentTarget.value;
+    var php = true;
+    var Javascript = true;
+    
+    switch (type) {
+
+      case 'type-all':
+      console.log('é todos')
+      var PHP = true;
+      var JavaScript = true;
+      break;
+
+      case 'type-PHP':
+      console.log('é php')
+      var PHP = true;
+      var JavaScript = false;
+      break;
+
+      case 'type-Javascript':
+        console.log('é javascript')
+      var PHP = false;
+      var JavaScript = true;
+      break;
+      
+      default:
+        
+    }
+
+        
+  };
+  
+  const [GitRepoApi, setGitRepoApi] = useState([]);
   useEffect(() => {
     axios.get(GitApiConfig.baseUrl+"/users/luan94/repos")
     .then(res => {
@@ -24,6 +61,7 @@ const Axios = () => {
       console.log(GitRepoApi)
     });
   }, []);
+  
 
   return (
     <div className="container">
@@ -40,16 +78,26 @@ const Axios = () => {
 
       
         
-      <div className="col-md-12 search-projetc-wrapper">                
+      <div className="col-md-12">
+        <div className="row search-projetc-wrapper">               
             <input className="search-repo-input"/>
             <button type="button" className="btn-repo-search button-type">Pesquisar</button>
         </div>
+      </div>
+      <div className="col-md-12">
+        <div className="filter-label">Filtar por Linguagem de Programação:</div>
+        <div className="row filter-btn-wrapper">
+          <button type="button" className="button-type" id="allBtn" value="type-all" onClick={handleClick} >Todos</button>
+          <button type="button" className="button-type" id="PHP-btn" value="type-PHP" onClick={handleClick}>PHP</button>          
+          <button type="button" className="button-type" id="Javascript-btn" value="type-Javascript" onClick={handleClick}>Javascript</button>
+        </div>
+      </div>
       
 
       
       <div className="row">
         {GitRepoApi.map(repo => (
-          <div className="col-md-4" key={repo.id}>
+          <div className={'col-md-4 all-cards' + repo.language } key={repo.id}>
             
               <div className="repo-card-wrapper">
                 <div className="col-md-12"><p className="repo-name-label">Nome do Projeto:</p></div>
